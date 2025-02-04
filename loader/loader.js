588a8463cc020208bdda965b5dda908898426cd3
donguyenphu <donguyenphu022008@gmail.com>
2025-02-04 16:30:46 +0700

    auto generated image slide

--- loader/loader.js
+++ loader/loader.js
@@ -1,15 +1,17 @@
 let loader = document.getElementById("preloader");
 let swipeRightWrapper = document.getElementById("swipeRightWrapper");
+let swipeLeftWrapper = document.getElementById("swipeLeftWrapper");
 const themeSwitch = document.getElementById('theme-switch');
 let darkMode = localStorage.getItem('darkmode');
 let imageMainForImages = document.getElementById("imageMainForImages");
+// preloader
 window.addEventListener('load', function() {
     setTimeout(function() {
         loader.style.display = 'none';
-    }, 0); 
+    }, 500); 
 });
 
-
+// darkmode
 const enableDark = () => {
     document.body.classList.add('darkmode');
     localStorage.setItem('darkmode', 'active');
@@ -35,9 +37,12 @@ themeSwitch.addEventListener("click", () => {
     }
 });
 
+// image container
 let tmp = 0;
 
 let arrImgLink = ['./images/myImages0.jpg', './images/myImages1.jpg', './images/myImages2.jpg'];
+// let arrImgLink = ['./images/AMEB4.JPG', './images/AMEB5.PNG', './images/AMEB6.JPG'];
+
 let length = arrImgLink.length;
 swipeRightWrapper.addEventListener("click", function () {
     console.log(tmp);
@@ -45,4 +50,20 @@ swipeRightWrapper.addEventListener("click", function () {
     tmp %= length;
     imageMainForImages.src = arrImgLink[tmp];
 });
+
+swipeLeftWrapper.addEventListener("click", function () {
+    console.log(tmp);
+    tmp = (!tmp ? length - 1 : tmp - 1);
+    imageMainForImages.src = arrImgLink[tmp];
+});
+
+function loopSwipe() {
+    imageMainForImages.src = arrImgLink[tmp];
+    tmp++;
+    tmp %= length;
+}
+
+setInterval(loopSwipe, 2500);
+
+
 
