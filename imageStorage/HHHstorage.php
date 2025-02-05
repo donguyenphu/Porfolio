22fbf09eda5328f3c1e860fe90addfd8be659871
donguyenphu <donguyenphu022008@gmail.com>
2025-02-05 19:27:16 +0700

    update HHH

---
+++ imageStorage/HHHstorage.php
@@ -0,0 +1,51 @@
+<?php
+    $level = '../';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once '../elements/head.php'; ?>
+    <title>Activities and Events</title>
+</head>
+<body>
+    <div id="preloader"></div>
+    <div class="container">
+        <?php require_once '../elements/header.php'; ?>
+        <div class="seperate"></div>
+        <div class="allWrapperDetails">
+            <!-- 6/2023 : hhh -->
+             <!-- 9/2023: icm -->
+                <!-- 2/2024 : endhhh -->
+              <!-- 5/2024: endicm -->
+            <div class="lineAndTitleWrapper">
+                <div class="aboutMeTitleWrapper">
+                    <h1 class="aboutMeTitleMain">My club's trip to Viet Duc hospital</h1>
+                </div>
+            </div>
+            <!-- 18 images -> 3 images each line -->
+            <div>
+                <div class="imageStorageWrapper">
+                    <div class="imageOnlyWrapper">
+                        <img src="../images/AMEB4.JPG" class="imageOnlyMain">
+                    </div>
+                    <div class="imageOnlyWrapper">
+                        <img src="../images/AMEB4.JPG" class="imageOnlyMain">
+                    </div>
+                    <div class="imageOnlyWrapper">
+                        <img src="../images/AMEB4.JPG" class="imageOnlyMain">
+                    </div>
+                    <div class="imageOnlyWrapper">
+                        <img src="../images/AMEB4.JPG" class="imageOnlyMain">
+                    </div>
+                </div>
+                <div class="lineImageStorage"></div>
+                <div class="lineImageStorage"></div>
+                <div class="lineImageStorage"></div>
+                <div class="lineImageStorage"></div>
+                <div class="lineImageStorage"></div>
+            </div>
+        </div>
+    </div>
+    <script src = "../loader/loader.js"></script>
+</body>
+</html>
